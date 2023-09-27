7709b7e164e4ec4d35e1ba95a12edc045915335b
Eclipse <simernin.mat@gmail.com>
2023-09-27 14:59:43 +0300

    first

--- 3 programs/Program.cs
+++ 3 programs/Program.cs
@@ -1,9 +1,89 @@
-﻿Console.WriteLine("Выберете программу:\n" +
+﻿using System;
+
+Console.WriteLine("Выберете программу:\n" +
     "1. Угадай число\n" +
     "2. Таблица умножения\n" +
     "3. Делители числа\n" +
     "4. Выход");
 string? number = "0";
+
+
+static void randnum()
+{
+    Random rnd = new Random();
+    int? value = rnd.Next(100);
+
+
+    int? num = 0;
+    while (num != value)
+    {
+        Console.WriteLine("Введите число");
+        num = int.Parse(Console.ReadLine());
+        if (num > value)
+        {
+            Console.WriteLine("Введенное число больше загаданного\n");
+        }
+        else if (num < value)
+        {
+            Console.WriteLine("Введенное число меньше загаданного\n");
+        }
+        else
+        {
+            Console.WriteLine("Поздравляю, вы угадали число!!!\n");
+            break;
+        }
+    }
+
+    Console.WriteLine("\n\n");
+
+
+}
+
+static void num_num()
+{
+
+    int[,] table = new int[9, 9];
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            table[i, j] = (i + 1) * (j + 1);
+        }
+    }
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            Console.Write(table[i, j] + "\t");
+        }
+        Console.WriteLine();
+
+
+    Console.WriteLine("\n\n");
+    }
+
+}
+
+static void dels()
+{
+    Console.Write("Введите число, для которого надо найти делители: ");
+    int? num = int.Parse(Console.ReadLine());
+    int? a = num;
+    while (a != 1)
+    {
+        if (num % a == 0)
+        {
+            Console.Write(a + " ");
+        }
+        a -= 1;
+
+    }
+    Console.WriteLine("\n\n");
+}
+
+
+
+
 
 
 do
@@ -12,70 +92,21 @@ do
     switch (number)
     {
         case "1":
-            Random rnd = new Random();
-            int? value = rnd.Next(100);
 
-
-            int? num = 0;
-            while (num != value)
-            {
-                Console.WriteLine("Введите число");
-                num = int.Parse(Console.ReadLine());
-                if (num > value)
-                {
-                    Console.WriteLine("Введенное число больше загаданного");
-                }
-                else if (num < value)
-                {
-                    Console.WriteLine("Введенное число меньше загаданного");
-                }
-                else
-                {
-                    Console.WriteLine("Поздравляю, вы угадали число!!!");
-                    break;
-                }
-            }
-
-            Console.WriteLine("\n\n");
+            randnum();
 
             break;
 
         case "2":
-            int[,] table = new int[9, 9];
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    table[i, j] = (i + 1) * (j + 1);
-                }
-            }
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write(table[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
 
+            num_num();
 
             break;
 
 
         case "3":
-            Console.WriteLine("Введите число, для которого надо найти делители");
-            num = int.Parse(Console.ReadLine());
-            int? a = num;
-            while (a != 1)
-            {
-                if (num % a == 0)
-                {
-                    Console.Write(a + " ");
-                }
-                a -= 1;
-
-            }
-            Console.WriteLine("\n\n");
+            
+            dels();
 
             break;
 
@@ -94,3 +125,5 @@ do
     }
 }
 while (number != "4");
+
+
